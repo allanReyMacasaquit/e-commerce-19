912e0ce9ed0584f3c58e26b423cca28e42881985
allan <allanrey.macasaquit@gmail.com>
2024-11-22 10:34:59 -0500

    2. setup Auth using Clerk nextjs

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+	return <div className='auth-center'>{children}</div>;
+};
+export default AuthLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 function Homepage() {
 	return (
 		<div className='bg-brand-dark'>
-			ADMIN DASHBOARD <Button className='btn-default'> Submit</Button>
+			<UserButton afterSwitchSessionUrl='/' />
 		</div>
 	);
 }
